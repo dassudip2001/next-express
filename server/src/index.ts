6aef9b874f562eae51ff2a17b06a7245abd531ba
Sudip Das <sudip5428@gmail.com>
2024-07-31 22:08:26 +0530

    code commit for ejs setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,24 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
-const app: Application = express();
+import path from "path";
+import { fileURLToPath } from "url";
+
 const PORT = process.env.PORT || 3000;
+const app: Application = express();
+
+// Correct way to set __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("server is running......");
+  return res.render("welcome");
 });
 
-app.listen(PORT, () => console.log(`server is running ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
